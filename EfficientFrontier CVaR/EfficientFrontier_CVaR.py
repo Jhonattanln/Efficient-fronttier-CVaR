d4350340be353526440f68823450b8010239d54f
Jhonattanln <63247251+Jhonattanln@users.noreply.github.com>
2020-08-22 20:32:55 -0300

    Current

--- EfficientFrontier CVaR/EfficientFrontier_CVaR.py
+++ EfficientFrontier CVaR/EfficientFrontier_CVaR.py
@@ -1,1 +1,18 @@
+import pandas as pd
+import numpy as np
+from pypfopt.efficient_frontier import EfficientFrontier
+#from pypfopt.objective_functions import negative_cvar
+from pypfopt.risk_models import CovarianceShrinkage
 
+##Importar série de dados
+df = pd.read_excel(r'C:\Users\Jhona\OneDrive\Área de Trabalho\PRBR11.xlsx', index_col='Data')
+
+##Obter os retornos dos ativos
+returns = pd.DataFrame()
+for i in df:
+    returns[i] = df[i].pct_change().dropna()
+
+##Criar a matrix de covariância eficiente
+covMatrix = CovarianceShrinkage(df)
+e_cov = covMatrix.ledoit_wolf()
+print(e_cov)
